93dd7c4501cec1684291d318df02fe288ca0a3d1
stefan-kanazir <kanazir.stefan@gmail.com>
2022-04-29 23:59:34 +0200

    Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <section className="flex flex-col py-12">
+            <div className="flex justify-center items-center mb-12">
+                <Image 
+                    src="/images/site/stefan.jpg"
+                    alt="Stefan Kanazir"
+                    width={300}
+                    height={300}
+                    className="rounded-full overflow-hidden"
+                />
+            </div>
+            <h1 className="text-5xl text-center font-semibold">Hi, I'm Stefan 👋</h1>
+            <p className="text-xl mt-6 text-center">I blog about Front End web development and UX/UI Design</p>
+        </section>
+    )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
+import Hero from "../components/Hero"
+
 const Homepage = () => {
     return (
-            <h1>Homepage</h1>
+        <>
+            <Hero />
+        </>
     )
 }
 
