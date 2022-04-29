f2fee66c550ca98c3c0f35913e545cbe515d1947
stefan-kanazir <kanazir.stefan@gmail.com>
2022-04-30 01:22:21 +0200

    Container styling - PostsGrid

--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -2,7 +2,7 @@ import PostsGrid from "./PostsGrid"
 
 const FeaturedPosts = ({ posts }) => {
     return (
-        <section className="py-12">
+        <section className="py-12 px-6 max-w-7xl mx-auto">
             <h2 className="text-4xl text-center mb-12 text-teal-700">Featured Posts</h2>
             <PostsGrid posts={posts} />
         </section>
--- components/PostsGrid.tsx
+++ components/PostsGrid.tsx
@@ -2,7 +2,7 @@ import PostItem from "./PostItem"
 
 const PostsGrid = ({ posts }) => {
     return (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {posts.map((post) => (
                 <PostItem 
                     key={post.slug}
