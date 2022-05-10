2570eda0d1b3c9631efe5e1bcf8570212604f4fc
stefan-kanazir <kanazir.stefan@gmail.com>
2022-05-10 23:06:32 +0200

    Rendering code snippets

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -1,6 +1,8 @@
 import PostHeader from "./PostHeader"
 import ReactMardown from "react-markdown"
 import Image from "next/image"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
 const customRenderers = {
     img(image) {
@@ -33,6 +35,18 @@ const customRenderers = {
         }
 
         return <p>{paragraph.children}</p>
+    },
+    code(code) {
+        const { className, children } = code;
+        const language = className.split('-')[1]; 
+
+        return (
+            <SyntaxHighlighter
+            style={atomDark}
+            language={language}
+            children={children}
+          />
+        )
     }
 }
 
