71650bcb0b4155b0b8af134c77ebd463f5ff8a1a
stefan-kanazir <kanazir.stefan@gmail.com>
2022-05-10 22:57:53 +0200

    Rendering images with ReactMarkdown

--- components/PostContent.tsx
+++ components/PostContent.tsx
@@ -1,12 +1,39 @@
 import PostHeader from "./PostHeader"
 import ReactMardown from "react-markdown"
+import Image from "next/image"
 
-const DUMMY_POST = {
-    slug: "getting-started-with-nextjs",
-    title: "Getting Started with NextJS",
-    image: "getting-started-with-nextjs.png",
-    date: "2022-04-30",
-    content: "# This is a first post",
+const customRenderers = {
+    img(image) {
+        return (
+            <Image 
+                src={image.src} 
+                alt={image.alt} 
+                width={600} 
+                height={300}
+                layout="responsive" 
+            />
+        )
+    },
+    p(paragraph) {
+        const { node } = paragraph;
+
+        if (node.children[0].type === 'img') {
+            const image = node.children[0];
+
+            return (
+                <div>
+                    <Image 
+                        src={image.src} 
+                        alt={image.alt} 
+                        width={600} 
+                        height={300} 
+                    />
+                </div>
+            )
+        }
+
+        return <p>{paragraph.children}</p>
+    }
 }
 
 const PostContent = ({post}) => {
@@ -14,7 +41,7 @@ const PostContent = ({post}) => {
         <article className="bg-gray-300 min-h-screen p-6">
             <main className="max-w-4xl mx-auto bg-gray-200 rounded-md p-10">
                 <PostHeader title={`${post.title}`} image={`/images/posts/${post.image}`} />
-                <ReactMardown className="block-content">
+                <ReactMardown className="block-content" components={customRenderers}>
                     {post.content}
                 </ReactMardown>
             </main>
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -8,11 +8,7 @@ isFeatured: true
 
 ## NextJS is a **framework for ReactJS**
 
-Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript?
-
-Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people.
-
-Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
+Wait a second ... a "framework" for React? Isn't React itself already a framework for JavaScript? Well ... first of all, React is a "library" for JavaScript. That seems to be important for some people. Not for me, but still, there is a valid point: React already is a framework / library for JavaScript. So it's already an extra layer on top of JS.
 
 ## Why would we then need NextJS?
 
