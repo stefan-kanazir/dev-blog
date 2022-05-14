45e87541a1b39e95df0a50dafc80e2f86db036af
stefan-kanazir <kanazir.stefan@gmail.com>
2022-05-14 23:22:01 +0200

    Handling notification messages on the Front End

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import { request } from "https";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Notification from "./Notification";
 
 const ContactForm = () => {
@@ -8,6 +8,15 @@ const ContactForm = () => {
     const [enteredMessage, setEnteredMessage] = useState("");
     const [requestStatus, setRequestStatus] = useState<"pending" | "success" | "error">()
     const [requestError, setRequestError] = useState();
+    
+    useEffect(() => {
+        if (requestStatus === "success" || requestStatus === "error") {
+            setTimeout(() => {
+                setRequestStatus(null)
+                setRequestError(null)
+            }, 2000)
+        }
+    }, [requestStatus, requestError])
     
     async function sendContacData(contactDetails) {
         const resposne = await fetch("/api/contact", {
@@ -37,6 +46,9 @@ const ContactForm = () => {
                 message: enteredMessage
             })
             setRequestStatus("success")
+            setEnteredMessage("")
+            setEnteredEmail("")
+            setEnteredName("")
         } catch (error) {
             setRequestError(error.message)
             setRequestStatus("error")
@@ -77,17 +89,17 @@ const ContactForm = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-3">
                         <div className="flex flex-col">
                             <label htmlFor="email" className="text-lg mb-1">Your Email</label>
-                            <input type="email" id="email" required className="py-1 rounded-md" value={enteredEmail} onChange={event => setEnteredEmail(event.target.value)} />
+                            <input type="email" id="email" required className="pl-3 py-1 rounded-md" value={enteredEmail} onChange={event => setEnteredEmail(event.target.value)} />
                         </div>
                         <div className="flex flex-col">
                             <label htmlFor="name" className="text-lg mb-1">Your Name</label>
-                            <input type="text" id="name" required className="py-1 rounded-md" value={entereddName} onChange={event => setEnteredName(event.target.value)} />
+                            <input type="text" id="name" required className="pl-3 py-1 rounded-md" value={entereddName} onChange={event => setEnteredName(event.target.value)} />
                         </div>
                     </div>
 
                     <div className="flex flex-col mb-6">
                         <label htmlFor="message" className="text-lg mb-1">Your Message</label>
-                        <textarea id="message" rows={5} required value={enteredMessage} onChange={event => setEnteredMessage(event.target.value)}></textarea>
+                        <textarea className="pl-3 pt-1" id="message" rows={5} required value={enteredMessage} onChange={event => setEnteredMessage(event.target.value)}></textarea>
                     </div>
 
                     <button type="submit" className="py-3 px-5 w-full md:w-auto bg-teal-700 hover:bg-teal-800 text-white font-bold rounded-md">Send Message</button>
