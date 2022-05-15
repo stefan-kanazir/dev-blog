bdd840960b5edfe91bb7d5c004b3647806bff0f5
stefan-kanazir <kanazir.stefan@gmail.com>
2022-05-15 23:16:22 +0200

    Using React Portals for notifications

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom';
 function Notification(props) {
   const { title, message, status } = props;
 
-//   let statusClasses = '';
-
-//   if (status === 'success') {
-//     statusClasses = classes.success;
-//   }
-
-//   if (status === 'error') {
-//     statusClasses = classes.error;
-//   }
-
-//   const cssClasses = `${classes.notification} ${statusClasses}`;
-
-  return (
-    <div className={`${status === "success" ? "bg-green-500" : ""} ${status === "error" ? "bg-red-500" : ""} ${status === "pending" ? "bg-blue-500" : ""} text-white mt-4 w-full p-6 rounded-md`}>
+  return ReactDOM.createPortal(
+    <div className={`${status === "success" ? "bg-green-500" : ""} ${status === "error" ? "bg-red-500" : ""} ${status === "pending" ? "bg-blue-500" : ""} text-white mt-4 w-full p-6 rounded-md max-w-2xl mx-auto`}>
       <h2 className="text-2xl mb-4">{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 }
 
